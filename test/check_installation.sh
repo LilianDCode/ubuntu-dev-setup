a370d4e9ef9f7df2930f215583ccb9b85508ff2e
Lilian Durand <liliandcode@gmail.com>
2024-05-20 22:16:12 +0200

    test: Test for vscode extensions added

--- test/check_installation.sh
+++ test/check_installation.sh
@@ -11,10 +11,37 @@ command -v warp-terminal >/dev/null 2>&1 && echo "Warp-terminal installed ✅" |
 command -v postman >/dev/null 2>&1 && echo "Postman installed ✅" || { echo "ERROR: postman not installed ❌"; SUCCESS_INSTALLATION=0; }
 
 if [ "$SUCCESS_INSTALLATION" -eq 1 ]; then
-    echo "✅ All tools are successfully installed!"
+    echo "All tools are successfully installed! ✅"
 else
     EXIT_CODE=1
     echo "Error during installation process. ❌ Please check logs for more details."
+fi
+
+# Check VSCode extensions if insllation successful
+
+if [ "$SUCCESS_INSTALLATION" -eq 1 ]; then
+    EXTENSIONS_INSTALLED=1
+
+    echo "Checking VSCode extensions..."
+
+    EXTENSIONS=$(code --list-extensions)
+    EXPECTED_EXTENSIONS="davidanson.vscode-markdownlint github.copilot github.copilot-chat ms-vscode-remote.remote-containers ms-vscode-remote.remote-ssh ms-vscode.remote-explorer ms-vsliveshare.vsliveshare pshaddel.conventional-branch streetsidesoftware.code-spell-checker vivaxy.vscode-conventional-commits yzhang.markdown-all-in-one"
+
+    for EXTENSION in $EXPECTED_EXTENSIONS; do
+        if ! echo "$EXTENSIONS" | grep -q "$EXTENSION"; then
+            echo "VS Code extension $EXTENSION not installed correctly. ❌"
+            EXTENSIONS_INSTALLED=0
+        else
+            echo "VS Code extension $EXTENSION installed ✅"
+        fi
+    done
+
+    if [ "$EXTENSIONS_INSTALLED" -eq 1 ]; then
+        echo "All VSCode extensions are successfully installed! ✅"
+    else
+        EXIT_CODE=1
+        echo "Error during installation of vscode extensions. ❌ Please check logs for more details."
+    fi
 fi
 
 # Keep the container running
